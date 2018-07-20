262dece9608e5886f3a67a70dafb812c92e3701c
Matthias Baidinger <s0552527@wh.f4.htw-berlin.de>
2018-07-20 18:25:12 +0200

    Fehlerbehebung

---
+++ dummy.cpp
@@ -0,0 +1,13 @@
+#include "dummy.hpp"
+
+void MouseFunc(int x, int y, int xx, int yy) {
+	//tu nix
+}
+
+void MouseMotionFunc(int x, int y) {
+	//tu nix
+}
+
+void MousePassiveMotionFunc(int x, int y) {
+	//tu nix
+}
---
+++ dummy.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef dummy_Header
+#define dummy_Header
+
+void MouseFunc(int x, int y, int xx, int);
+
+void MouseMotionFunc(int x, int y);
+
+void MousePassiveMotionFunc(int x, int y);
+
+#endif
